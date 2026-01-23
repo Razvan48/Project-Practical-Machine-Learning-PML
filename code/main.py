5f2372f8d340c5b5bf955806d297c18753e93bc4
Răzvan Nicolae Căpățînă <razvannicolae48@gmail.com>
2026-01-23 23:50:00 +0200

    .

--- code/main.py
+++ code/main.py
@@ -33,8 +33,13 @@ warnings.filterwarnings('ignore')
 # trainings.train_fuzzy_c_mean_0('../data', dataset_type='fasttext')
 
 
-# visualizations.visualize_birch_0('..', model_name='birch_tf_idf_0.21613335888101168', dataset_type='tf_idf')
-# visualizations.visualize_fuzzy_c_mean_0('..', model_name='fuzzy_c_means_fasttext_0.18106917033914544', dataset_type='fasttext')
+visualizations.visualize_birch_0('..', model_name='birch_tf_idf_0.2263843648208469', dataset_type='tf_idf')
+# visualizations.visualize_fuzzy_c_mean_0('..', model_name='fuzzy_c_means_tf_idf_0.24822762981414065', dataset_type='tf_idf')
+
+# visualizations.visualize_birch_0('..', model_name='birch_fasttext_0.10011496455259629', dataset_type='fasttext')
+# visualizations.visualize_fuzzy_c_mean_0('..', model_name='fuzzy_c_means_fasttext_0.18729641693811075', dataset_type='fasttext')
+
+
 
 
 
